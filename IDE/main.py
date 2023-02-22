463e7d3ee78e095975931493ce0f69eb007a5680
Official-BReep <67698695+Official-BReep@users.noreply.github.com>
2023-02-22 19:09:32 +0100

    added string and link highlight

--- IDE/main.py
+++ IDE/main.py
@@ -5,7 +5,6 @@ from PyQt5.QtGui import QTextCharFormat, QFont, QFontDatabase
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPlainTextEdit
 from highlighter import Highlighter
 
-
 class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
@@ -29,9 +28,31 @@ class Main(QMainWindow):
 
         output_format = QTextCharFormat()
         output_format.setForeground(Qt.GlobalColor.darkRed)
-        #output_format.setFontWeight(QFont.Weight.Bold)
-        pattern = r'^\s*out\s*\(\b.*\b\)$'
+        output_format.setFontWeight(QFont.Weight.Bold)
+        pattern = r'^\s*out\s*\(\w*|.*\)$'
         self.highlighter.add_mapping(pattern, output_format)
+
+        define_format = QTextCharFormat()
+        define_format.setForeground(Qt.GlobalColor.blue)
+        define_format.setFontWeight(QFont.Weight.Bold)
+        pattern = r'define\s*\(\".*|w*\"\)'
+        self.highlighter.add_mapping(pattern, define_format)
+
+        output_text_format = QTextCharFormat()
+        output_text_format.setForeground(Qt.GlobalColor.black)
+        pattern = r'\(w*\)$'
+        self.highlighter.add_mapping(pattern, output_text_format)
+
+        string_format = QTextCharFormat()
+        string_format.setForeground(Qt.darkGreen)
+        pattern = r'"\w*\"\,*|;*$'
+        self.highlighter.add_mapping(pattern, string_format)
+
+        link_format = QTextCharFormat()
+        link_format.setForeground(Qt.blue)
+        pattern = r'^(?:https?://)?(?:[\w]+\.)(?:\.?[\w]{2,})+$'
+        self.highlighter.add_mapping(pattern, link_format)
+
 
         self.editor = QPlainTextEdit()
 
