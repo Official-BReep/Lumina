f5c63599b132c45dd0f372fa48c184341720a8f2
BReep <67698695+Official-BReep@users.noreply.github.com>
2023-05-01 22:25:45 +0200

    IDE in C# instead of Python
    I decided to write the IDE with C# instead of Python, on the one hand C# runs faster and on the other hand I see more possibilities to expand the whole thing

---
+++ IDE/IDE/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace IDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lock = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.rtxtB = new System.Windows.Forms.RichTextBox();
+            this.Tabs = new System.Windows.Forms.TabControl();
+            this.addnewtab = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage1.SuspendLayout();
+            this.Tabs.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lock
+            // 
+            this.Lock.Location = new System.Drawing.Point(907, 593);
+            this.Lock.Name = "Lock";
+            this.Lock.Size = new System.Drawing.Size(85, 23);
+            this.Lock.TabIndex = 1;
+            this.Lock.Text = "🔓 Read Only";
+            this.Lock.UseVisualStyleBackColor = true;
+            this.Lock.Click += new System.EventHandler(this.Lock_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AllowDrop = true;
+            this.tabPage1.Controls.Add(this.rtxtB);
+            this.tabPage1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(828, 540);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // rtxtB
+            // 
+            this.rtxtB.AcceptsTab = true;
+            this.rtxtB.AutoWordSelection = true;
+            this.rtxtB.Location = new System.Drawing.Point(0, 3);
+            this.rtxtB.Name = "rtxtB";
+            this.rtxtB.Size = new System.Drawing.Size(832, 544);
+            this.rtxtB.TabIndex = 0;
+            this.rtxtB.Text = "";
+            this.rtxtB.TextChanged += new System.EventHandler(this.rtxtB_TextChanged);
+            // 
+            // Tabs
+            // 
+            this.Tabs.Controls.Add(this.tabPage1);
+            this.Tabs.Controls.Add(this.addnewtab);
+            this.Tabs.Location = new System.Drawing.Point(156, 25);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.SelectedIndex = 0;
+            this.Tabs.Size = new System.Drawing.Size(836, 566);
+            this.Tabs.TabIndex = 2;
+            this.Tabs.TabStop = false;
+            // 
+            // addnewtab
+            // 
+            this.addnewtab.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.addnewtab.Location = new System.Drawing.Point(4, 22);
+            this.addnewtab.Name = "addnewtab";
+            this.addnewtab.Size = new System.Drawing.Size(828, 540);
+            this.addnewtab.TabIndex = 1;
+            this.addnewtab.Text = "+";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1004, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 619);
+            this.ControlBox = false;
+            this.Controls.Add(this.Lock);
+            this.Controls.Add(this.Tabs);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.Tabs.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Lock;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox rtxtB;
+        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TabPage addnewtab;
+    }
+}
+
---
+++ IDE/IDE/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace IDE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Tabs.Selected += new TabControlEventHandler(Tabs_Selected);
+            WebClient webclient = new WebClient();
+            try
+            {
+                if (!webclient.DownloadString("https://raw.githubusercontent.com/Triskix/Lumina/main/version.txt").Contains("Version"))
+                {
+                    if (MessageBox.Show("An Update is available, do you want to download it?", "IDE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        using (var client = new WebClient())
+                        {
+                            Process.Start("Updater.exe");
+                            this.Close();
+                        }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtxtB_TextChanged(object sender, EventArgs e)
+        {
+
+            Regex rx = new Regex("class");
+            int index = rtxtB.SelectionStart;
+
+            foreach (Match match in rx.Matches(rtxtB.Text))
+            {
+                rtxtB.Select(match.Index, match.Value.Length);
+                rtxtB.SelectionColor = Color.Blue;
+                rtxtB.SelectionStart = index;
+                rtxtB.SelectionColor = Color.Black;
+            }
+
+            Regex rx2 = new Regex("out");
+            foreach (Match match in rx2.Matches(rtxtB.Text))
+            {
+                rtxtB.Select(match.Index, match.Value.Length);
+                rtxtB.SelectionColor = Color.Orange;
+                rtxtB.SelectionStart = index;
+                rtxtB.SelectionColor = Color.Black;
+            }
+
+            Regex rx3 = new Regex("define");
+            foreach (Match match in rx3.Matches(rtxtB.Text))
+            {
+                rtxtB.Select(match.Index, match.Value.Length);
+                rtxtB.SelectionColor = Color.BlueViolet;
+                rtxtB.SelectionStart = index;
+                rtxtB.SelectionColor = Color.Black;
+            }
+
+        }
+
+        private void Lock_Click(object sender, EventArgs e)
+        {
+            if (rtxtB.ReadOnly)
+            {
+                rtxtB.ReadOnly = false;
+                Lock.Text = "🔓 Read Only";
+            }
+            else
+            {
+                rtxtB.ReadOnly = true;
+                Lock.Text = "🔒 Read Only";
+            }
+        }
+        private void Tabs_Selected(object sender, EventArgs e)
+        {
+            if (Tabs.SelectedTab.Text == "+")
+            {
+                TabPage myTabPage = new TabPage("Tab "+ Tabs.TabCount);
+                Tabs.TabPages.Add(myTabPage);
+                Tabs.SelectedTab = myTabPage;
+                Tabs.TabPages.Remove(addnewtab);
+                addnewtab = new TabPage("+");
+                Tabs.TabPages.Add(addnewtab);
+
+            }
+        }
+    }
+}
