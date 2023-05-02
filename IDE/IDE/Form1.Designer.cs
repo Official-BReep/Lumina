1c193a45041b3ac2db1f3f1d533ae4faa9c64104
BReep <67698695+Official-BReep@users.noreply.github.com>
2023-05-02 20:53:06 +0200

    added Some Functions und some Bugs haha

--- IDE/IDE/Form1.Designer.cs
+++ IDE/IDE/Form1.Designer.cs
@@ -38,15 +38,24 @@
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hexEditorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tabPage1.SuspendLayout();
             this.Tabs.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Lock
             // 
-            this.Lock.Location = new System.Drawing.Point(907, 593);
+            this.Lock.Location = new System.Drawing.Point(1080, 658);
             this.Lock.Name = "Lock";
             this.Lock.Size = new System.Drawing.Size(85, 23);
             this.Lock.TabIndex = 1;
@@ -62,7 +71,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(828, 540);
+            this.tabPage1.Size = new System.Drawing.Size(996, 607);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -71,9 +80,10 @@
             // 
             this.rtxtB.AcceptsTab = true;
             this.rtxtB.AutoWordSelection = true;
-            this.rtxtB.Location = new System.Drawing.Point(0, 3);
+            this.rtxtB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtB.Location = new System.Drawing.Point(3, 3);
             this.rtxtB.Name = "rtxtB";
-            this.rtxtB.Size = new System.Drawing.Size(832, 544);
+            this.rtxtB.Size = new System.Drawing.Size(990, 601);
             this.rtxtB.TabIndex = 0;
             this.rtxtB.Text = "";
             this.rtxtB.TextChanged += new System.EventHandler(this.rtxtB_TextChanged);
@@ -82,10 +92,10 @@
             // 
             this.Tabs.Controls.Add(this.tabPage1);
             this.Tabs.Controls.Add(this.addnewtab);
-            this.Tabs.Location = new System.Drawing.Point(156, 25);
+            this.Tabs.Location = new System.Drawing.Point(161, 19);
             this.Tabs.Name = "Tabs";
             this.Tabs.SelectedIndex = 0;
-            this.Tabs.Size = new System.Drawing.Size(836, 566);
+            this.Tabs.Size = new System.Drawing.Size(1004, 633);
             this.Tabs.TabIndex = 2;
             this.Tabs.TabStop = false;
             // 
@@ -101,10 +111,11 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1004, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1189, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -113,8 +124,7 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.newToolStripMenuItem,
             this.openToolStripMenuItem,
-            this.saveToolStripMenuItem,
-            this.saveAsToolStripMenuItem});
+            this.saveToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
@@ -127,30 +137,84 @@
             // 
             // openToolStripMenuItem
             // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.folderToolStripMenuItem});
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
-            // saveAsToolStripMenuItem
+            // openFileDialog1
             // 
-            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveAsToolStripMenuItem.Text = "Save As ...";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.fileToolStripMenuItem1.Text = "File";
+            this.fileToolStripMenuItem1.Click += new System.EventHandler(this.fileToolStripMenuItem1_Click);
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.folderToolStripMenuItem.Text = "Folder";
+            this.folderToolStripMenuItem.Click += new System.EventHandler(this.folderToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hexEditorToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // hexEditorToolStripMenuItem
+            // 
+            this.hexEditorToolStripMenuItem.Name = "hexEditorToolStripMenuItem";
+            this.hexEditorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hexEditorToolStripMenuItem.Text = "Hex Editor";
+            this.hexEditorToolStripMenuItem.Click += new System.EventHandler(this.hexEditorToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 29);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(142, 619);
+            this.treeView1.TabIndex = 4;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Controls.Add(this.Lock);
+            this.groupBox1.Controls.Add(this.Tabs);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1189, 696);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.UseCompatibleTextRendering = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 619);
+            this.ClientSize = new System.Drawing.Size(1189, 720);
             this.ControlBox = false;
-            this.Controls.Add(this.Lock);
-            this.Controls.Add(this.Tabs);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
@@ -163,6 +227,7 @@
             this.Tabs.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,8 +243,16 @@
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.TabPage addnewtab;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hexEditorToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- IDE/IDE/Form1.cs
+++ IDE/IDE/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -94,13 +95,151 @@ namespace IDE
         {
             if (Tabs.SelectedTab.Text == "+")
             {
-                TabPage myTabPage = new TabPage("Tab "+ Tabs.TabCount);
+                String title = "Tab" + Tabs.TabCount;
+                TabPage myTabPage = new TabPage(title);
+                var newTextField = new RichTextBox();
+                newTextField.Dock = DockStyle.Fill;
+                myTabPage.Controls.Add(newTextField);
+                myTabPage.Name = title;
+
+                myTabPage.Controls.Add(newTextField);
+                Tabs.TabPages.Add(myTabPage);
+                Tabs.SelectedTab = myTabPage;
+
+                Tabs.TabPages.Remove(addnewtab);
+                Tabs.TabPages.Add(addnewtab);
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RichTextBox myTB = Tabs.SelectedTab.Controls[0] as RichTextBox;
+            var Text = myTB.Text;
+            byte[] bytes = Encoding.ASCII.GetBytes(Text);
+            Console.WriteLine(Text);
+
+            saveFileDialog1.Filter = "RichTextFormate|*.rtf|Text Files|*.txt|All Files|*.*";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    fs.WriteByte(bytes[i]);
+                }
+
+                fs.Close();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog1.FileName;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                    rtxtB.Text = fileContent;
+                }
+            }
+        }
+
+        private void PopulateTreeView(string path, TreeNode parentNode)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            TreeNode node = new TreeNode(directory.Name);
+
+            if (parentNode == null)
+            {
+                treeView1.Nodes.Add(node);
+            }
+            else
+            {
+                parentNode.Nodes.Add(node);
+            }
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                PopulateTreeView(subDirectory.FullName, node);
+            }
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                node.Nodes.Add(file.Name);
+            }
+        }
+
+        private void fileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = openFileDialog1.FileName;
+                Console.WriteLine(filePath);
+                byte[] bytesData;
+                string stringData;
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    bytesData = new byte[fileStream.Length];
+                    fileStream.Read(bytesData, 0, (int)fileStream.Length);
+                }
+
+                stringData = System.Text.Encoding.UTF8.GetString(bytesData);
+                Console.WriteLine(stringData);
+
+                TabPage myTabPage = new TabPage("Tab " + Tabs.TabCount);
+                RichTextBox rtxtB2 = new RichTextBox();
+                myTabPage.Controls.Add(rtxtB2);
+                rtxtB2.Dock = DockStyle.Fill;
                 Tabs.TabPages.Add(myTabPage);
                 Tabs.SelectedTab = myTabPage;
                 Tabs.TabPages.Remove(addnewtab);
                 addnewtab = new TabPage("+");
                 Tabs.TabPages.Add(addnewtab);
 
+                RichTextBox myTB = Tabs.SelectedTab.Controls[0] as RichTextBox;
+                //var Text = myTB.Text;
+                myTB.Text = stringData;
+            }
+        }
+
+        private void hexEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hexEditorToolStripMenuItem.CheckOnClick = true;
+            if (hexEditorToolStripMenuItem.Checked)
+            {
+                hexEditorToolStripMenuItem.Checked = true;
+            }
+            else
+            {
+                hexEditorToolStripMenuItem.Checked = false;
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.FullPath;
+            Console.WriteLine(path);
+        }
+
+        private void folderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string root = folderBrowserDialog1.SelectedPath;
+                PopulateTreeView(root, null);
             }
         }
     }
